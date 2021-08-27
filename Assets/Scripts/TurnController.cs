cf7706dfa90f8b3a8fa9c6b05c26c4bf5860cd36
tornadoblot <newclearplant@gmail.com>
2021-08-28 06:00:38 +0900

    이동 관련 기동력 내용 추가

--- Assets/Scripts/TurnController.cs
+++ Assets/Scripts/TurnController.cs
@@ -7,12 +7,6 @@ public class TurnController : MonoBehaviour
     private bool isPlayerTurn = true;
     [SerializeField] private int mb;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
@@ -22,16 +16,27 @@ public class TurnController : MonoBehaviour
         }
     }
 
+
+    /// <summary>
+    /// 기동력을 한칸 소모합니다
+    /// </summary>
     public void MinusMb()
     {
         mb--;
     }
 
+    /// <summary>
+    /// 현재 플레이어 턴 여부를 리턴합니다
+    /// </summary>
+    /// <returns></returns>
     public bool IsPlayerTurn()
     {
         return isPlayerTurn;
     }
 
+    /// <summary>
+    /// 적 턴이 끝나고 현재 플레이어턴 확보, 기동력을 회복합니다
+    /// </summary>
     public void EndEnemyTurn()
     {
         mb = 6;
