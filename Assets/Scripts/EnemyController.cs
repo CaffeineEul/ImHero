c7be16a2180aa4728eb39f5d1419cd3ea790cf04
tornadoblot <newclearplant@gmail.com>
2021-08-28 05:32:25 +0900

    푸시 안된 EnemyController

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,14 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : Object
+public class EnemyController : Operator
 {
-    CharacterInfo characterInfo;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        characterInfo = GameObject.Find("Character").GetComponent<CharacterInfo>();
-
-    }
+    public Class me;
 }
