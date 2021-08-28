ebe507825d7a10f060a47fbb2445739f94b0b2ed
wlvlrtm <kimsk200012@gmail.com>
2021-08-28 14:22:46 +0900

    SoundController 추가

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    public GameObject[] Charactors;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
