a08a9842fc30b317061c1ea1df585dd001342bcb
tornadoblot <newclearplant@gmail.com>
2021-08-28 18:08:22 +0900

    맵 푸시

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -4,12 +4,5 @@ using UnityEngine;
 
 public class PlayerAttack : CharacterInfo
 {
-    MapInfo mapInfo;
-
-    private void Awake()
-    {
-        mapInfo = GameObject.Find("MapInfo").GetComponent<MapInfo>();
-    }
-
 
 }
