762744f0a613271d95d3be27c7af03328c7f3e5a
PooHappy <av3rs1on@naver.com>
2021-08-28 23:30:39 +0900

    123
    123

--- Assets/Scripts/EnemyAutoAttack.cs
+++ Assets/Scripts/EnemyAutoAttack.cs
@@ -20,25 +20,32 @@ public class EnemyAutoAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!turnController.IsPlayerTurn() && WaitTime)
+        if (!turnController.IsPlayerTurn() && WaitTime)
             StartCoroutine(Action());
         if (turnController.IsPlayerTurn())
+        {
             StopAllCoroutines();
+            WaitTime = true;
+        }
     }
     IEnumerator Action()
     {
+        WaitTime = false;
         foreach (var i in gameObjects)
         {
             characterInfo = i.GetComponent<CharacterInfo>();
             for(int j = 0; j < i.transform.childCount; j++)
             {
                 i.transform.GetChild(j).gameObject.SetActive(true);
-                if(i.transform.GetChild(j).gameObject.GetComponent<TileColChk>().isPlayer)
+
+                if (i.transform.GetChild(j).gameObject.GetComponent<TileColChk>().isPlayer)
                 {
                     characterInfo.Attack(i.transform.GetChild(j).gameObject.GetComponent<TileColChk>().player.GetComponent<Operator>(), characterInfo.GetDamage());
                     turnController.MinusMb();
-                    WaitTime = false;
-                    yield return new WaitForSeconds(1f);
+                    if (turnController.IsPlayerTurn())
+                        yield return null;
+                    else
+                        yield return new WaitForSeconds(1f);
                 }
             }
         }
@@ -55,41 +62,57 @@ public class EnemyAutoAttack : MonoBehaviour
                     case 0:
                         if (characterInfo.ReturnPositionV() > 0)
                         {
-                            characterInfo.objectMoveAlgorithm.GetMoveDir(h, v, h, v - 1);
-                            objectMoveAlgorithm.MoveTileMap(1);
+                            int ran = Random.Range(0, characterInfo.Areas.Length / 4);
+                            characterInfo.objectMoveAlgorithm.GetMoveDir(h, v, h, Mathf.Clamp(v - ran, 0, 7));
+                            objectMoveAlgorithm.MoveTileMap(ran);
                             turnController.MinusMb();
                             WaitTime = false;
-                            yield return new WaitForSeconds(1f);
+                            if (turnController.IsPlayerTurn())
+                                yield return null;
+                            else
+                                yield return new WaitForSeconds(1f);
                         }
                         break;
                     case 1:
                         if (characterInfo.ReturnPositionV() < 7)
                         {
-                            characterInfo.objectMoveAlgorithm.GetMoveDir(h, v, h, v + 1);
-                            objectMoveAlgorithm.MoveTileMap(1);
+                            int ran = Random.Range(0, characterInfo.Areas.Length / 4);
+                            characterInfo.objectMoveAlgorithm.GetMoveDir(h, v, h, Mathf.Clamp(v + ran, 0, 7));
+                            objectMoveAlgorithm.MoveTileMap(ran);
                             turnController.MinusMb();
                             WaitTime = false;
-                            yield return new WaitForSeconds(1f);
+                            if (turnController.IsPlayerTurn())
+                                yield return null;
+                            else
+                                yield return new WaitForSeconds(1f);
                         }
                         break;
                     case 2:
                         if (characterInfo.ReturnPositionH() > 0)
                         {
-                            characterInfo.objectMoveAlgorithm.GetMoveDir(h, v, h - 1, v);
-                            objectMoveAlgorithm.MoveTileMap(1);
+                            int ran = Random.Range(0, characterInfo.Areas.Length / 4);
+                            characterInfo.objectMoveAlgorithm.GetMoveDir(h, v, Mathf.Clamp(h-ran, 0,  7), v);
+                            objectMoveAlgorithm.MoveTileMap(ran);
                             turnController.MinusMb();
                             WaitTime = false;
-                            yield return new WaitForSeconds(1f);
+                            if (turnController.IsPlayerTurn())
+                                yield return null;
+                            else
+                                yield return new WaitForSeconds(1f);
                         }
                         break;
                     case 3:
                         if (characterInfo.ReturnPositionH() < 7)
                         {
-                            characterInfo.objectMoveAlgorithm.GetMoveDir(h, v, h + 1, v);
-                            objectMoveAlgorithm.MoveTileMap(1);
+                            int ran = Random.Range(0, characterInfo.Areas.Length / 4);
+                            characterInfo.objectMoveAlgorithm.GetMoveDir(h, v, Mathf.Clamp(h + ran, 0, 7), v);
+                            objectMoveAlgorithm.MoveTileMap(ran);
                             turnController.MinusMb();
                             WaitTime = false;
-                            yield return new WaitForSeconds(1f);
+                            if (turnController.IsPlayerTurn())
+                                yield return null;
+                            else
+                                yield return new WaitForSeconds(1f);
                         }
                         break;
                     default: 
@@ -97,7 +120,10 @@ public class EnemyAutoAttack : MonoBehaviour
                 }
             }
         }
-        yield return new WaitForSeconds(1f);
+        if (turnController.IsPlayerTurn())
+            yield return null;
+        else
+            yield return new WaitForSeconds(1f);
         WaitTime = true;
     }
 }
