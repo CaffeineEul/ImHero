31ec125952b59e8078800113a74fea57e6fd1a2b
tornadoblot <newclearplant@gmail.com>
2021-08-28 16:54:31 +0900

    캐릭터 인포 수정

--- Assets/Scripts/CharacterInfo.cs
+++ Assets/Scripts/CharacterInfo.cs
@@ -165,7 +165,7 @@ public class CharacterInfo : Operator
 
     public void MovePositionH(int var)
     {
-        if(mapInfo.Exist[Mathf.Clamp(h + var, 0, h + var), v])
+        if(mapInfo.Exist[Mathf.Clamp(h + var, 0, 7), v])
         {
             return;
         }
@@ -179,7 +179,7 @@ public class CharacterInfo : Operator
 
     public void MovePositionV(int var)
     {
-        if (mapInfo.Exist[h, Mathf.Clamp(v + var, 0, v + var)])
+        if (mapInfo.Exist[h, Mathf.Clamp(v + var, 0, 7)])
         {
             return;
         }
