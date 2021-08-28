b5c34364bb7508e89fb51740bbf165f04b15e1ec
wlvlrtm <kimsk200012@gmail.com>
2021-08-29 07:20:02 +0900

    다시 시작, FX 수정

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
@@ -181,9 +182,11 @@ public class UIController : MonoBehaviour
 
     public void Restart()
     {
+        SceneManager.LoadScene("Main");
         MenuWindow.SetActive(false);
         var = 1f;
         AllStop = false;
+        
     }
 
 
